81071278f236579a67416bf072ca0109d4517808
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-04-17 12:06:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	if (num < 0 || num > 50) {
+    return null;
+  } else if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let prevPrev = 0;
+    let prev = 1;
+    let current;
+    for (let i = 2; i <= num; i++) {
+      current = prevPrev + prev;
+      prevPrev = prev;
+      prev = current;
+    }
+    return current;
+  }
 }
 
 module.exports = fibonacci;
